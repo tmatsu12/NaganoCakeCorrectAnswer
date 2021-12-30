e18ba0c760ec75af75e6f99a34c35802be97a76c
tmatsu <tmatsunaga65@gmail.com>
2021-12-30 08:53:16 +0000

    [Add]Adminのログイン画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+  end
 end
