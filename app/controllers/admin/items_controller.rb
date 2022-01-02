86288c888eb26d42579aeb28f11fcae8972bd454
tmatsu <tmatsunaga65@gmail.com>
2022-01-02 10:45:34 +0000

    [Add]indexアクションの作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,18 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+
   def new
   end
 
   def index
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      all_items = @genre.items
+    else
+      all_items = Item.includes(:genre)
+    end
+    @items = all_items.page(params[:page])
+    @all_items_count = all_items.count
   end
 
   def show
