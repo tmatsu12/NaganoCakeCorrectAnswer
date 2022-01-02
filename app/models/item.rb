23f274837fae684e0f8647cca6bb8286170d3cab
tmatsu <tmatsunaga65@gmail.com>
2022-01-02 11:02:32 +0000

    [Add]genre,itemモデルファイルとデータの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :genre
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  attachment :image
+
+  scope :where_genre_active, -> { joins(:genre).where(genres: { is_active: true }) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,57 @@ Admin.create!(
   email: "a@a",
   password: "aaaaaa"
 )
+Genre.create!(
+  [
+    {
+      name: "ケーキ",
+      is_active: true,
+    },
+    {
+      name: "焼き菓子",
+      is_active: true,
+    },
+    {
+      name: "プリン",
+      is_active: false,
+    },
+    {
+      name: "和菓子",
+      is_active: false,
+    },
+    {
+      name: "アイス",
+      is_active: true,
+    }
+  ]
+  )
+for i in 1..10 do
+  Item.create!(
+    [
+      {
+        genre_id: 1,
+        name: "ケーキ#{i}",
+        introduction: "#{3*i-2}番目の商品です。",
+        price: rand(1..10) * 100,
+        image: File.open("./app/assets/images/cake#{i%5+1}.jpg"),
+        is_active: true
+      },
+      {
+        genre_id: 2,
+        name: "クッキー#{i}",
+        introduction: "#{3*i-1}番目の商品です。",
+        price: rand(1..10) * 200,
+        image: File.open("./app/assets/images/cookies#{i%5+1}.jpg"),
+        is_active: true
+      },
+      {
+        genre_id: 3,
+        name: "プリン#{i}",
+        introduction: "#{3*i}番目の商品です。",
+        price: rand(1..10) * 300,
+        image: File.open("./app/assets/images/pudding#{i%5+1}.jpg"),
+        is_active: false
+      }
+    ]
+    )
+end
