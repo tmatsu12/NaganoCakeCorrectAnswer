57c1a062856053d91b0c518af195b0beb7511118
tmatsu <tmatsunaga65@gmail.com>
2022-01-04 12:27:46 +0000

    [Add]商品詳細ページの作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a4d76b1a60a74ece7c336350499593dd1868ae5626248721fdc94c1cabc010cf80f17fec378e5e8a5c47b332720cce3d240547e8631ae515ca17a84bae2801db'
