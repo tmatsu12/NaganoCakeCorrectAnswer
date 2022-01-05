187197a6441f3ec4b40a8914d92a209a4cb5f5d5
tmatsu <tmatsunaga65@gmail.com>
2022-01-05 10:54:09 +0000

    [Add]会員関連のルーティングとバリデーションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  devise_for :customers
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
@@ -12,6 +6,7 @@ Rails.application.routes.draw do
   }
   namespace :admin do
     get 'top' => 'homes#top', as: 'top'
+    resources :customers, only: [:index, :show, :edit, :update]
     resources :items, except: [:destroy]
     resources :orders, only: [:index, :show, :update] do
     end
