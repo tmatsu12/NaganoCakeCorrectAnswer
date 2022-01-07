8b60d04f08936b0da39fc20987236eed82d97ede
tmatsu <tmatsunaga65@gmail.com>
2022-01-07 10:18:59 +0000

    [Add]会員詳細ページの作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin!
-  
+  before_action :ensure_customer, only: [:show, :edit, :update]
+
   def index
     @customers = Customer.page(params[:page])
   end
@@ -9,5 +10,11 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def ensure_customer
+    @customer = Customer.find(params[:id])
   end
 end
