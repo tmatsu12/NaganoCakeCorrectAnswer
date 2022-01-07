4fb8a03872a99fd39a80bde91eebcc4d2934a493
tmatsu <tmatsunaga65@gmail.com>
2022-01-07 10:23:09 +0000

    [Add]会員編集ページの作成とupdateアクションの作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,7 +12,15 @@ class Admin::CustomersController < ApplicationController
   def edit
   end
 
+  def update
+    @customer.update(customer_params) ? (redirect_to admin_customer_path(@customer)) : (render :edit)
+  end
+
   private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
+  end
 
   def ensure_customer
     @customer = Customer.find(params[:id])
