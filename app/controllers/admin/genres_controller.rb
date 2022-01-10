7123256d4ad5c5f8983533349d09a9ee204e1bca
tmatsu <tmatsunaga65@gmail.com>
2022-01-10 10:51:01 +0000

    [Add]ジャンル管理ページの作成

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,11 @@
+class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def edit
+  end
+end
