d8812ecefcd2350cc30952262fedb6a8611c1cbe
tmatsu <tmatsunaga65@gmail.com>
2022-01-17 11:44:21 +0000

    [Add]マイページの作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,7 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :set_current_customer
+
   def edit
   end
 
@@ -6,5 +9,11 @@ class Public::CustomersController < ApplicationController
   end
 
   def unsubscribe
+  end
+
+  private
+
+  def set_current_customer
+    @customer = current_customer
   end
 end
