7a8d3ef6f1f3659b8f204e03a490696be6eb1d33
tmatsu <tmatsunaga65@gmail.com>
2022-01-23 06:57:53 +0000

    [Add]カートアイテム一覧ページの作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,4 +5,31 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = current_customer.cart_items.includes(:item)
   end
+
+  def create
+    if @cart_item
+      new_amount = @cart_item.amount + cart_item_params[:amount]
+      @cart_item.update(amount: new_amount)
+      redirect_to cart_items_path
+    else
+      @cart_item = current_customer.cart_items.new(cart_item_params)
+      @cart_item.item_id = @item.id
+      if @cart_item.save
+        redirect_to cart_items_path
+      else
+        render 'public/items/show'
+      end
+    end
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:amount)
+  end
+
+  def set_cart_item
+    @item = Item.find(params[:item_id])
+    @cart_item = current_customer.has_in_cart(@item)
+  end
 end
